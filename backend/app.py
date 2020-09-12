a14447a4028a8a801f0b2f4a88857caf37f89f0d
repl.it user <replituser@example.com>
2020-09-12 00:17:28 +0000

    Complete part 5

--- backend/app.py
+++ backend/app.py
@@ -59,9 +59,9 @@ def get_all_restaurants():
     minRating = int(minRating)
     filtered_restaurants = []
     for check_restaurant in db.get('restaurants'):
-      if check_restaurant["rating"] >= minRating:
+      if check_restaurant.get("rating") >= minRating:
         filtered_restaurants.append(check_restaurant)
-    
+
     if not filtered_restaurants:
       return create_response(status=404, message="No restaurants are found with this rating or above")
     return create_response({"restaurants": filtered_restaurants})
@@ -69,11 +69,16 @@ def get_all_restaurants():
 @app.route("/restaurants", methods=['POST'])
 def create_restaurant():
   new_restaurant_data = request.get_json()
-  if new_restaurant_data["name"] is None:
+
+  if new_restaurant_data.get("name") is None:
     return create_response(status=422, message="Restraunt name not provided")
-  elif new_restaurant_data["rating"] is None:
+  elif new_restaurant_data.get("rating") is None:
     return create_response(status=422, message="Restraunt rating not provided")
-  new_restaurant = db.create('restaurants', new_restaurant_data)
+
+  new_payload = {"name": new_restaurant_data.get("name"),
+   "rating": int(new_restaurant_data.get("rating"))}
+  new_restaurant = db.create('restaurants', new_payload)
+
   return create_response(status=201, data=new_restaurant)
 
 @app.route("/restaurants/<id>", methods = ['GET'])
@@ -81,6 +86,21 @@ def get_restaurant(id):
   if db.getById('restaurants', int(id)) is None:
     return create_response(status=404, message="No restaurant with this id exists")
   return create_response(db.getById('restaurants', int(id)))
+
+@app.route("/restaurants/<id>", methods = ['PUT'])
+def update_restaurant(id):
+  if db.getById('restaurants', int(id)) is None:
+    return create_response(status=404, message="No restaurant with this id exists")
+
+  update_restaurant_data = request.get_json()
+  if not update_restaurant_data.get("name") is None:
+    new_name_payload = {"name": update_restaurant_data.get("name")}
+    db.updateById('restaurants', int(id), new_name_payload)
+
+  if not update_restaurant_data.get("rating") is None:
+    new_rating_payload = {"rating": int(update_restaurant_data.get("rating"))}
+    db.updateById('restaurants', int(id), new_rating_payload)
+  return create_response(status=201, data=db.getById('restaurants', int(id)))
 
 @app.route("/restaurants/<id>", methods=['DELETE'])
 def delete_restaurant(id):
