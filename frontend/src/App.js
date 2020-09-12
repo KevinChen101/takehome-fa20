0af1280297ca13a9fda8adb45b4d938a68bfa4ad
repl.it user <replituser@example.com>
2020-09-12 03:08:43 +0000

    Finish challenge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      inputName: '',
       restaurants: [
         {id: 1, name: "Golden Harbor", rating: 10},
         {id: 2, name: "Potbelly", rating: 6},
@@ -23,9 +24,24 @@ class App extends Component {
         {this.state.restaurants.map(x => (
           <Restaurant id={x.id} name={x.name} rating={x.rating} />
         ))}
+
+        <input value={this.state.inputName} onChange={evt => this.updateInputName(evt)}/>
+        <button type="button" onClick={this.addRestaurant}>Submit</button>
       </div>
     )
   }
+
+  updateInputName(evt) {
+    this.setState({
+      inputName: evt.target.value
+    });
+  }
+
+  addRestaurant = () => {
+    this.setState((prevState) => ({restaurants: [...prevState.restaurants,
+     {id: this.state.restaurants.length + 1,
+     name: this.state.inputName, rating: 0}]}))
+  } 
 }
 
 export default App
